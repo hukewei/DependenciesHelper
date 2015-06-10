9a872918c679b29b5763d7197ac7dca8a5edeb7c
kewei <keweihu@yahoo.com>
2015-06-10 22:44:32 +0200

    ADD::sort function

--- js/main.js
+++ js/main.js
@@ -37,4 +37,31 @@ $(document).ready( function() {
             $( "#hidden_block").hide();
         }
     });
+    $( "#sort" ).click(function() {
+        //clear the previous text
+        var matched_deps = [];
+        var lines = $('#text_to_convert').val().split('\n');
+        for(var i = 0;i < lines.length;i++){
+            var cleaned_text = lines[i].replace(/^ +/gm, '');
+            if(cleaned_text != '') {
+                matched_deps.push(cleaned_text);
+                console.log(cleaned_text);
+            }
+        }
+        console.log(matched_deps.length);
+        if(matched_deps.length > 0) {
+            matched_deps.sort();
+            var sorted_text = matched_deps[0];
+            for (var index = 1; index < matched_deps.length; index++) {
+                sorted_text += '\n' + matched_deps[index];
+            }
+            $( "#hidden_info").hide();
+            $( "#hidden_block").show();
+            console.log(sorted_text);
+            $('#textToCopy').text(sorted_text);
+        } else {
+            $( "#hidden_info").show();
+            $( "#hidden_block").hide();
+        }
+    });
 });
